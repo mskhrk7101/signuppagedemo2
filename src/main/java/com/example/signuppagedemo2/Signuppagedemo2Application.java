e4b22f9361a7ca91ccdeb401166e2586b3fad7cc
mskhrk7101 <msk19970709@gmail.com>
2022-08-07 08:01:21 +0900

    first commit

---
+++ src/main/java/com/example/signuppagedemo2/Signuppagedemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.signuppagedemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Signuppagedemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Signuppagedemo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/signuppagedemo2/Signuppagedemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signuppagedemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Signuppagedemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
